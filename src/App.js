133eb60bc6b100102d36597a9ddaa0eea22c5683
Drew Brantley <brantley@us.ibm.com>
2019-09-06 16:13:32 -0500

    formatting page

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
       title:"Objective",
       content:[
         {
-          type:"",
-          data:[],
+          type:"plain",
+          data:["Obtain a Software Engineering position at IBM"],
         }
       ]
     },
@@ -20,17 +20,60 @@ function App() {
       title:"Work Experience",
       content:[
         {
-          type:"",
-          data:[],
-        }
+          type:"plain-date",
+          data:["IBM – IT Specialist"],
+          date:"September 2016-present"
+        },
+        {
+          type:"bullet",
+          data:[
+            "Worked with two teams on IBM Worklight Apps using HTML, CSS, and JS",
+            "Backend for Mobile App in Node.JS that is now published ",
+            "Support for NYPD bodycam proposal ",
+            "Presented at IBM Seller’s Academy and IBM Insight conferences ",
+            "Made UI for internal IBM search application",
+          ],
+        },
+        {
+          type:"plain-date",
+          data:["IBM – IT Specialist Co-Op, Coppell, Texas"],
+          date:"Summer 2014, May 2015-September 2016"
+        },
+        {
+          type:"bullet",
+          data:[
+            "Worked with two teams on IBM Worklight Apps using HTML, CSS, and JS",
+            "Backend for Mobile App in Node.JS that is now published ",
+            "Support for NYPD bodycam proposal ",
+            "Presented at IBM Seller’s Academy and IBM Insight conferences ",
+            "Made UI for internal IBM search application",
+          ],
+        },
+        {
+          type:"plain-date",
+          data:["Camp Counselor - Camp Gilmont - Gilmer, Texas"],
+          date:""
+        },
+        {
+          type:"bullet",
+          data:[
+            "Responsible for leading campers from the age of 7 to 17 in camp related activities",
+            "Trained campers to become counselor",
+          ],
+        },
       ]
     },
     {
       title:"Skills",
       content:[
         {
-          type:"",
-          data:[],
+          type:"plain",
+          data:[
+            "Software: Word, Excel, PowerPoint, Eclipse, Android Studio, Bluemix, Git",
+            "Languages and Technologies: Java, HTML, JavaScript, CSS, NodeJS, Cloudant",
+            "Operating Systems: Unix, Windows, Mac",
+            "Education: University of Texas at Dallas– Software Engineering Major",
+          ],
         }
       ]
     },
@@ -38,8 +81,9 @@ function App() {
       title:"Achievements",
       content:[
         {
-          type:"",
-          data:[],
+          type:"plain-date",
+          data:["Eagle Scout"],
+          date:""
         }
       ]
     },
--- src/components/templates/section.js
+++ src/components/templates/section.js
@@ -5,7 +5,7 @@ import './style.css'
 function Section(props){
 
     return (
-        <div>
+        <div className="section">
             {/* <div>{JSON.stringify(props,null,2)}</div> */}
             <SectionTitle></SectionTitle>
             <SectionContent></SectionContent>
@@ -13,10 +13,52 @@ function Section(props){
     );
 
     function SectionTitle(){
-        return (<div  className="title">{props.title}</div>);
+        return (<div  className="section-title">{props.title}</div>);
     }
     function SectionContent(){
-        return (<div></div>);
+
+        const {content} = props;
+
+        let to_return = content.map((single_content)=>{
+
+            let single_to_return;
+
+            if(single_content.type==="plain"){
+                
+                single_to_return = single_content.data.map((cur)=>{
+                    return(
+                        <div>{cur}</div>
+                    )
+                });
+
+            }else if(single_content.type==="plain-date"){
+
+                single_to_return = (
+                    <span className="plain-date-holder">
+                        <span className="title">{single_content.data[0]}</span>
+                        <span className="date">{single_content.date || "ADD A DATE"}</span>
+                    </span>
+                );
+
+            }else if(single_content.type==="bullet"){
+
+                single_to_return = single_content.data.map((cur)=>{
+                    return (
+                        <div>* {cur}</div>
+                    );
+                });
+
+            }else{
+                console.log(JSON.stringify(single_content,null,2));
+                debugger
+                single_to_return = JSON.stringify(single_content,null,2)
+            }
+
+            return single_to_return;
+
+        });
+
+        return (<div className="section-content">{to_return}</div>);
 
         function subSection(){
             
