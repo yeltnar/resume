6d04cb845e1f6510c5af84a58dd1df79e2527cbd
Drew Brantley <brantley@us.ibm.com>
2021-11-30 17:15:27 -0600

    new save pdf

--- .pdf_save.sh
+++ .pdf_save.sh
@@ -1,8 +1,17 @@
-KEYBASE_LOCATION="/Volumes/Keybase"
-
 export path="./pdf/resume.pdf" 
-export url="file://$KEYBASE_LOCATION/public/droodle_b/resume/index.html" 
+export build_dir="file://$PWD/build/"
+export url="$build_dir/index.html" 
 
+# make back up of current build folder
+mv build .build.bk
+
+# build with fs aware routing
+PUBLIC_URL="$build_dir" yarn run build &&
+
+# use print to pdf node module 
 node print/index.js
 
-cp -r pdf/* $KEYBASE_LOCATION/public/droodle_b/resume/
+echo "saved to $path"
+
+# replace build folder with previous content 
+mv .build.bk build
