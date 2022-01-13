82d32db1a8f580653b9702640222c0cc35c4e583
Drew Brantley <brantley@us.ibm.com>
2022-01-13 11:10:27 -0600

    quick deploy

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b210dc8e20324d277e70a5e73240830b.js"
+  "/precache-manifest.5374cd44bbb4912aaf4444d443326080.js"
 );
 
 self.addEventListener('message', (event) => {
