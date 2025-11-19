0803befa3b63581596dfb6b1d3b1d32fde148a50
drew <github@andbrant.com>
2025-11-18 22:55:15 -0600

    move to vite

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // Optionally configure aliases, server options, etc. here
+});
