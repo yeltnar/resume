277064a061c0a2357ea349f4028d89e5cb4c1100
drew <github@andbrant.com>
2025-11-19 14:45:13 -0600

    mor changes for making pdf

--- .pdf_save.sh
+++ .pdf_save.sh
@@ -1,13 +1,16 @@
+# THIS REQUIRES THE BUILD TO HAVE ALREADY HAPPEN
+
 export path="./pdf/AndrewBrantleyResume.pdf" 
 export build_dir="file://$PWD/dist/"
 export url="http://localhost:9999" 
 
 # make back up of current build folder
-rm -rf .dist.bk
-mv dist .dist.bk
+# rm -rf .dist.bk
+# mv dist .dist.bk
 
 # build with fs aware routing
-PUBLIC_URL="$build_dir" npm run build &&
+# PUBLIC_URL="$build_dir" npm run build &&
+npm run build &&
 
 podman rm -f pdf_builder; podman run --name pdf_builder -p 9999:9999 -v .:/host -d node:latest npx serve /host/dist -p 9999 --no-port-switching
 # start_pid="$!"
@@ -23,4 +26,4 @@ podman rm -f pdf_builder
 echo "saved to $path"
 
 # replace build folder with previous content 
-mv .dist.bk dist
+# mv .dist.bk dist
